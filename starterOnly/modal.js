3bb30dd730a6a8f8e983bb431f1d60fdbcdb668d
woengus <olivierbiard@woengus.com>
2022-06-30 10:52:14 +0200

    creation js rules for modal closing

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+const closeBtn = document.querySelector(".close"); //bouton croix sur la modal
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +20,9 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+//Au click sur la croix on appelle la function closeModal
+closeBtn.addEventListener("click", closeModal);
+//fermeture de la modal
+function closeModal() {
+  modalbg.style.display = "none";
+}
